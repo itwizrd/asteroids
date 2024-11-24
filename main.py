555e2bebb07af1e9d1461dc56d429680b6fc51af
itwizrd <29903092+itwizrd@users.noreply.github.com>
2024-11-24 09:49:46 -0500

    rotate & movement

--- main.py
+++ main.py
@@ -9,15 +9,15 @@ def main():
     
     pygame.init()
     clock = pygame.time.Clock()
-    dt = 0
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
     player = Player(SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2)
+    dt = 0
 
     while True:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return
-            
+        player.update(dt)
         screen.fill("black")
         player.draw(screen)
         pygame.display.flip()
