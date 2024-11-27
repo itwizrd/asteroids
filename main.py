430458cb091a424e3766fa58cf336d56df6bf287
itwizrd <29903092+itwizrd@users.noreply.github.com>
2024-11-26 22:51:14 -0500

    typo and added asteroids

--- main.py
+++ main.py
@@ -1,37 +1,45 @@
 import pygame
 from constants import  *
 from player import *
+from asteroid import Asteroid
+from asteroidfield import AsteroidField
 
 def main():
     print("Starting asteroids!")
     print(f"Screen width: {SCREEN_WIDTH}")
     print(f"Screen height: {SCREEN_HEIGHT}")
-    # we put things in groups so that we can call them all at once
-    updateable = pygame.sprite.Group()
-    drawable = pygame.sprite.Group()
-    Player.containers = (updateable, drawable)
-
-    
     pygame.init()
-    clock = pygame.time.Clock()
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
-    player = Player(SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2)
+    clock = pygame.time.Clock()
     dt = 0
+
+    # we put things in groups so that we can call them all at once
+    updatable = pygame.sprite.Group()
+    drawable = pygame.sprite.Group()
+    asteroids = pygame.sprite.Group()
+
+    Player.containers = (updatable, drawable)
+    Asteroid.containers = (asteroids, updatable, drawable)
+    AsteroidField.containers = (updatable,) #extra comma makes a tuple
+
+    player = Player(SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2)
+    asteroid_field = AsteroidField()
 
     while True:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return
-        for i in updateable:
-            i.update(dt)
+        for obj in updatable:
+            obj.update(dt)
         # clear what was drawn in previous frame and prepare for the next rendering
         # this goes before the rendering because its assoicated with draw/rendering
         screen.fill("black") 
-        for i in drawable:
-            i.draw(screen)
+        for obj in drawable:
+            obj.draw(screen)
 
         pygame.display.flip()
         
+        # limit framerate to 60FPS
         dt = (clock.tick(60)/1000)
     
     
