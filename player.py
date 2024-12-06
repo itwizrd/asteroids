5fa30cc4c5f5d8f18c76b901cad6f213452d8f8e
itwizrd <29903092+itwizrd@users.noreply.github.com>
2024-12-05 20:27:11 -0500

    rof limit

--- player.py
+++ player.py
@@ -7,6 +7,7 @@ class Player(CircleShape):
         super().__init__(x, y, PLAYER_RADIUS)
         self.radius = PLAYER_RADIUS
         self.rotation = 0
+        self.timer = 0
 
     def triangle(self):
         forward = pygame.Vector2(0, 1).rotate(self.rotation)
@@ -26,8 +27,12 @@ class Player(CircleShape):
         self.position += forward * PLAYER_SPEED * dt
     def shoot(self):
         velocity = pygame.Vector2(0, 1).rotate(self.rotation) * PLAYER_SHOOT_SPEED
-        shot = Shot(self.position.x, self.position.y, velocity)
-        return shot
+        if self.timer <= 0:
+            shot = Shot(self.position.x, self.position.y, velocity)
+            self.timer = PLAYER_SHOOT_COOLDOWN
+            return shot
+        return
+
 
 
     
@@ -44,3 +49,4 @@ class Player(CircleShape):
             self.move(-dt)
         if keys[pygame.K_SPACE]:
             self.shoot()
+        self.timer = max(0, self.timer - dt)
